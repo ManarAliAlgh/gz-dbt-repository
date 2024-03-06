4102c7ae9eccc30a0946d94bf6cc1b8bcb450140
Manar Alghamdi <manar.ali.algh@gmail.com>
2024-03-06 07:19:45 +0000

    commit_sales

--- models/intermediate/int_sales_margin.sql
+++ models/intermediate/int_sales_margin.sql
@@ -1,12 +1,12 @@
-SELECT 
-     products_id,
-     date_date,
-     orders_id,
-     revenue,
-     quantity,
-     CAST(purchase_price AS FLOAT64) AS price,
-     ROUND(s.quantity*CAST(p.purchase_price AS FLOAT64),2) AS purchase_cost,
-     s.revenue - ROUND(s.quantity*CAST(purchase_price AS FLOAT64),2) AS margin 
-FROM {{ref("stg_raw__sales")}} s 
-LEFT JOUN {{ref("stg_raw__product")}} p 
-    USING (products_id)
+SELECT
+    s.products_id AS products_id,
+    s.date_date,
+    s.orders_id,
+    s.revenue,
+    s.quantity,
+    CAST(p.purchase_price AS FLOAT64) AS purchase_price,
+    ROUND(s.quantity * CAST(p.purchase_price AS FLOAT64),2) AS purchase_cost,
+    s.revenue - ROUND(s.quantity * CAST(p.purchase_price AS FLOAT64),2) AS margin
+FROM {{ ref("stg_raw__sales") }} s
+LEFT JOIN {{ ref("stg_raw__product") }} p
+    ON s.products_id= p.products_id
